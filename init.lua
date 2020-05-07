9704352821db8a273d05d8ca3f56c75fd4e807b4
IIIullaIII <alzir.ulla@gmail.com>
2020-05-07 18:46:49 +0200

    update

--- init.lua
+++ init.lua
@@ -3,13 +3,28 @@
 minetest.register_alias("canapa","cannabis:canapa")
 
 --tools
-local path = minetest.get_modpath("cannabis")
+cannabis = {}       -- A global Object for the mod
+cannabis.path = minetest.get_modpath("cannabis") -- The path of the mod
+local path = cannabis.path
+
+-- Get Boilerplate to translate
+local S
+if(minetest.get_translator) then
+   S = minetest.get_translator(minetest.get_current_modname())
+
+else
+    S = function ( s ) return s end
+
+end
+
+cannabis.S = S  -- Stores the boilerplate for global access
+
 dofile(path.."/tools.lua")
 dofile(path.."/canapa.lua")
 dofile(path.."/node_ingot.lua")
 dofile(path.."/craftitem.lua")
 dofile(path.."/armor.lua")
---dofile(path.."/joint.lua")   --for joint if want enable   Per abilitare la sigaretta di canapa    
+dofile(path.."/joint.lua")
 dofile(path.."/canapa_rossa.lua")
 dofile(path.."/craftred.lua")
 dofile(path.."/rope.lua")
